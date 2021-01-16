61db0ba3d980e2dc69f3ef02206ef10e9c0a2a84
Donovan Hutchinson <donovanh@gmail.com>
2021-01-16 21:55:52 +0000

    Adding workflows

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # What day is it?
+
+![Today is...](https://hop.ie/today/day.png)
+
+Unless you're in a timezone where the above is incorrect. If so, [try the live site!](https://hop.ie/today/)
+
+## How? Why? Wait, what?
+
+This is a demo to show how a Github Action can create a dynamic social media sharing image, on a schedule. Read the full post on how it's done here (TBD!).
+
+## License
+
+To thrill!
---
+++ screenshot.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('https://hop.ie/today/');
+  await wait(1500);
+  await page.setViewport({
+    width: 1200,
+    height: 675,
+    deviceScaleFactor: 1,
+  });
+  await page.screenshot({
+    path: './site/today.png'
+  });
+  console.log('Screenshot written');
+  await browser.close();
+})();
